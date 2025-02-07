ec0972a1e4ad7e1b53bf1dd6759b71d44780cf28
Rohankhandal <rohankhandal84@gmail.com>
2025-02-07 21:35:03 +0530

    Login and SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import { LogIn } from "./pages/LogIn";
+import { SignUp } from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <p>EduNexus</p>
-    </>
+    <Router>
+      <AppContainer>
+        {/* <h1>EduNexus</h1> */}
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f4f4f4;
+`;
